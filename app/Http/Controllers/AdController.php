38dd1aafb8271249ccd357d0771a244fbfbf9280
maciekstudzinski <maciek@hrefagency.co.uk>
2024-03-21 00:11:42 +0000

    filter refactor - query saved into a function - local scope query

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -12,21 +12,10 @@ class AdController extends Controller
      */
     public function index()
     {
-        $ads = Ad::query();
+        $filters = request()->only('search', 'min_salary', 'max_salary', 'experience', 'category');
+        $ads = Ad::filter($filters);
 
-        $ads->when(request('search'), function($query) {
-            $query->where(function ($query) {
-                $query->where('title', 'like', '%' . request('search') . '%')->orWhere('description', 'like', '%' . request('search') . '%');
-            });
-        })->when(request('min_salary'), function($query) {
-            $query->where('salary', '>=', request('min_salary'));
-        })->when(request('max_salary'), function($query) {
-            $query->where('salary', '<=', request('max_salary'));
-        })->when(request('experience'), function($query) {
-            $query->where('experience',  request('experience'));
-        })->when(request('category'), function($query) {
-            $query->where('category',  request('category'));
-        });
+     
 
         return view('ad.index', ['ads' => $ads->get()]);
     }
