f26fd17c8b7bd53bf96f2710136cafeda4ada288
maciekstudzinski <maciek@hrefagency.co.uk>
2024-03-22 00:05:28 +0000

    Created models and factories for Employer class, updated seeder

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Ad extends Model
@@ -28,5 +29,9 @@ class Ad extends Model
         })->when($filters['category'] ?? null, function($query, $category) {
             $query->where('category',  $category);
         });
+    }
+
+    public function employer():BelongsTo {
+        return $this->belongsTo(Employer::class);
     }
 }
---
+++ app/Models/Employer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Employer extends Model
+{
+    use HasFactory;
+
+    public function ads():HasMany {
+        return $this->hasMany(Ad::class);
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}
